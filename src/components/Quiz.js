c9491b256400607335aa7e9b69e653ab05944bac
Simplifier <iseldomused@gmail.com>
2020-02-18 05:07:06 +0300

    remove title

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,41 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import QuestionList from './QuestionList';
 
-const Quiz = ({ step, questions, totalQuestions, score, handleSingleAnswer, handleMultiAnswer }) => {
-  return (
-    <div className="wrapper">
-      <header>
-        <div className="question-count">
-          <h2>Question</h2>
-          <div className="question-number">{step}</div>
-          <div className="description">of <span>{totalQuestions}</span></div>
+const Quiz = ({step, questions, totalQuestions, score, handleSingleAnswer, handleMultiAnswer}) => {
+    return (
+        <div className="wrapper">
+            <div className="question-count">
+                <h2>Question</h2>
+                <div className="question-number">{step}</div>
+                <div className="description">of <span>{totalQuestions}</span></div>
+            </div>
+            <div className="questions">
+                <QuestionList
+                    questions={questions}
+                    handleSingleAnswer={handleSingleAnswer}
+                    handleMultiAnswer={handleMultiAnswer}
+                />
+            </div>
+            <div className="score-container">
+                <h2>Score</h2>
+                <div className="score">{score}</div>
+                <div className="description">points</div>
+            </div>
         </div>
-        <h1>Java Quiz</h1>
-        <div className="score-container">
-          <h2>Score</h2>
-          <div className="score">{score}</div>
-          <div className="description">points</div>
-        </div>
-      </header>
-
-      <div className="questions">
-        <QuestionList
-          questions={questions}
-          handleSingleAnswer={handleSingleAnswer}
-          handleMultiAnswer={handleMultiAnswer}
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 Quiz.propTypes = {
-  step: PropTypes.number.isRequired,
-  questions: PropTypes.array.isRequired,
-  totalQuestions: PropTypes.number.isRequired,
-  score: PropTypes.number.isRequired,
-  handleSingleAnswer: PropTypes.func.isRequired,
-  handleMultiAnswer: PropTypes.func.isRequired
+    step: PropTypes.number.isRequired,
+    questions: PropTypes.array.isRequired,
+    totalQuestions: PropTypes.number.isRequired,
+    score: PropTypes.number.isRequired,
+    handleSingleAnswer: PropTypes.func.isRequired,
+    handleMultiAnswer: PropTypes.func.isRequired
 };
 
 export default Quiz;
